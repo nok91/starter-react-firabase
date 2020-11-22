6f0c24d70065eec75948d7499ff3de1c51903226
Mohammed Nokri <mohammed.nokri@itg.co.uk>
2020-11-22 12:35:38 +0000

    💅🏽 STYLE: Cosmetic changes using tailwind style

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import PostForm from '../../components/PostForm';
+
+function Home() {
+    return (
+        <div className="h-full">
+            <PostForm />
+        </div>
+    )
+}
+
+export default Home
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#cec0b5',
+        primaryLight: '#fae6cd',
+        primaryDark: '#9a785c',
+        secondary: '#2f3b2d',
+      },
+      fontFamily: ['Quattrocento'],
+    },
   },
   variants: {
     extend: {},
